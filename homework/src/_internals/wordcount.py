1638567132ee68e6e3eda6d784859d7e874bd4da
Jaime Manuel Figueroa <99105750+jaimemanuelf@users.noreply.github.com>
2025-12-06 07:50:41 -0500

    refactored

--- homework/src/_internals/wordcount.py
+++ homework/src/_internals/wordcount.py
@@ -4,7 +4,11 @@
 
 import argparse
 
+from homework.src._internals.count_words import count_words
+from homework.src._internals.preprocess_lines import preprocess_lines
 from homework.src._internals.read_all_lines import read_all_lines
+from homework.src._internals.split_into_words import split_into_words
+from homework.src._internals.write_word_counts import write_word_counts
 
 
 def parse_args():
@@ -21,25 +25,6 @@ def parse_args():
     parsed_args = parser.parse_args()
 
     return parsed_args.input, parsed_args.output
-
-
-def preprocess_lines(lines):
-    return [line.strip().lower() for line in lines]
-
-
-def split_into_words(lines):
-    words = []
-    for line in lines:
-        words.extend(words.strip(",.!?") for words in line.split())
-    return words
-
-
-def count_words(words):
-    pass
-
-
-def write_word_counts(output_folder, word_counts):
-    pass
 
 
 def main():
